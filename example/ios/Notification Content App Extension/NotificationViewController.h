34bfafa247be1a4db8764ed79e51d4c52c1779f5
Nikita Tsvirko <nikita.tsvirko@altabel.com>
2020-05-25 01:41:13 +0300

    Added Notification Content Extension.

---
+++ example/ios/Notification Content App Extension/NotificationViewController.h
@@ -0,0 +1,12 @@
+//
+//  NotificationViewController.h
+//  Notification Content App Extension
+//
+//  Created by Никита Цвирко on 5/24/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ example/ios/Notification Content App Extension/NotificationViewController.m
@@ -0,0 +1,27 @@
+//
+//  NotificationViewController.m
+//  Notification Content App Extension
+//
+//  Created by Никита Цвирко on 5/24/20.
+//
+
+#import "NotificationViewController.h"
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface NotificationViewController () <UNNotificationContentExtension>
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any required interface initialization here.
+}
+
+- (void)didReceiveNotification:(UNNotification *)notification {
+    
+}
+
+@end
